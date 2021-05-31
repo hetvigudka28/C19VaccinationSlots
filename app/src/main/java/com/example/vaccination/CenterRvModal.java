3514c252d09724c67edacee2a720fa153d0ba4e1
katniss2810 <katniss2810@users.noreply.github.com>
2021-05-31 14:33:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaccination/CenterRvModal.java
@@ -0,0 +1,15 @@
+package com.example.vaccination;
+
+public class CenterRvModal {
+
+    String centerName;
+    String centerAddress;
+    String centerFromTime;
+    String centerToTime;
+    String fee_type;
+    int ageLimit;
+    String vaccineName;
+    int availableCapacity;
+
+
+}
