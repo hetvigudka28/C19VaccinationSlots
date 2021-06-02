ed19ec76f8fd28dc0922bcb081e4bc139e939c04
katniss2810 <katniss2810@users.noreply.github.com>
2021-06-02 10:26:26 +0530

    Clipart icons, Layout, Model Class

--- app/src/main/java/com/example/vaccination/CenterRvModal.java
+++ app/src/main/java/com/example/vaccination/CenterRvModal.java
@@ -1,6 +1,9 @@
 package com.example.vaccination;
 
-public class CenterRvModal {
+import java.util.Objects;
+
+public class CenterRvModal
+{
 
     String centerName;
     String centerAddress;
@@ -10,6 +13,19 @@ public class CenterRvModal {
     int ageLimit;
     String vaccineName;
     int availableCapacity;
+
+    public CenterRvModal(String centerName, String centerAddress, String centerFromTime, String centerToTime, String fee_type, int ageLimit, String vaccineName, int availableCapacity)
+    {
+        this.centerName=centerName;
+        this.centerAddress=centerAddress;
+        this.centerFromTime=centerFromTime;
+        this.centerToTime=centerToTime;
+        this.fee_type=fee_type;
+        this.ageLimit=ageLimit;
+        this.vaccineName=vaccineName;
+        this.availableCapacity=availableCapacity;
+
+    }
 
 
 }
